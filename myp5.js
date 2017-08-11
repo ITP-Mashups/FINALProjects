661a2f7b7e93a5e30d39ea13bac057ce535751a4
rbs517 <rebeccaskurnik@gmail.com>
2017-08-11 19:32:35 -0400

    Files

---
+++ myp5.js
@@ -0,0 +1,207 @@
+var lyricObjects =[];
+
+var searchResults1, searchResults2;
+
+
+function setup() {
+  // create canvas
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.parent('myCanvasDiv');
+  textAlign(CENTER);
+  textSize(20);
+}
+
+function draw() {
+   //image(bgimage, 0, 0, window.innerWidth, window.innerHeight);
+  // if there is anything in the global lyrics variable
+  background(255);
+  if (lyricObjects.length >0) {
+    for (var i=0; i< lyricObjects.length; i++){
+        lyricObjects[i].run();  
+    }
+  } 
+
+  if (searchResults1) {
+    fill(0);
+    textSize(32);
+    text(searchResults1, 0, 0);
+
+  } 
+   if (searchResults2) {
+    fill(0);
+    textSize(32);
+    text(searchResults2, 0, 0);
+
+  } 
+}
+
+function loadLyricsOnce() {
+  for (var i=0; i< lyrics.length; i++){
+      lyricObjects[i]= new Lyric(random(width),random(height), lyrics[i]);
+  }
+
+}
+
+
+
+function Lyric(x,y,_text){  
+  this.acceleration = createVector(0, 0);
+  this.velocity = p5.Vector.random2D();
+  this.position = createVector(x, y);
+  this.r = 3.0;
+  this.maxspeed = 3;    // Maximum speed
+  this.maxforce = 0.05; // Maximum steering force
+  this.text=_text;
+
+}
+
+Lyric.prototype.run = function() {
+  //this.flock(lyrics);
+  this.update();
+  this.borders();
+  this.render();
+};
+
+// Forces go into acceleration
+Lyric.prototype.applyForce = function(force) {
+  this.acceleration.add(force);
+};
+
+// We accumulate a new acceleration each time based on three rules
+/*Lyric.prototype.flock = function(lyrics) {
+  var sep = this.separate(lyrics); // Separation
+  var ali = this.align(lyrics);    // Alignment
+  var coh = this.cohesion(lyrics); // Cohesion
+  // Arbitrarily weight these forces
+  sep.mult(2.5);
+  ali.mult(1.0);
+  coh.mult(1.0);
+  // Add the force vectors to acceleration
+  this.applyForce(sep);
+  this.applyForce(ali);
+  this.applyForce(coh);
+};*/
+
+// Method to update location
+Lyric.prototype.update = function() {
+  // Update velocity
+  this.velocity.add(this.acceleration);
+  // Limit speed
+  this.velocity.limit(this.maxspeed);
+  this.position.add(this.velocity);
+  // Reset acceleration to 0 each cycle
+  this.acceleration.mult(0);
+};
+
+// A method that calculates and applies a steering force towards a target
+// STEER = DESIRED MINUS VELOCITY
+Lyric.prototype.seek = function(target) {
+  var desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
+  // Normalize desired and scale to maximum speed
+  desired.normalize();
+  desired.mult(this.maxspeed);
+  // Steering = Desired minus Velocity
+  var steer = p5.Vector.sub(desired, this.velocity);
+  steer.limit(this.maxforce); // Limit to maximum steering force
+  return steer;
+};
+
+// Draw boid as a circle--lyrics
+Lyric.prototype.render = function() {
+  fill(119,191,199);
+  stroke(200);
+  //text(this.text, this.position.x, this.position.y);
+  //ellipse(this.position.x, this.position.y, 16, 16);
+  text(this.text, this.position.x, this.position.y, 90, 76 );
+};
+
+// Wraparound
+Lyric.prototype.borders = function() {
+  if (this.position.x < -this.r) this.position.x = width + this.r;
+  if (this.position.y < -this.r) this.position.y = height + this.r;
+  if (this.position.x > width + this.r) this.position.x = -this.r;
+  if (this.position.y > height + this.r) this.position.y = -this.r;
+};
+
+// Separation
+// Method checks for nearby boids and steers away
+Lyric.prototype.separate = function(lyrics) {
+  var desiredseparation = 25.0;
+  var steer = createVector(0, 0);
+  var count = 0;
+  // For every boid in the system, check if it's too close
+  for (var i = 0; i < lyrics.length; i++) {
+    var d = p5.Vector.dist(this.position, lyrics[i].position);
+    // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+    if ((d > 0) && (d < desiredseparation)) {
+      // Calculate vector pointing away from neighbor
+      var diff = p5.Vector.sub(this.position, lyrics[i].position);
+      diff.normalize();
+      diff.div(d); // Weight by distance
+      steer.add(diff);
+      count++; // Keep track of how many
+    }
+  }
+  // Average -- divide by how many
+  if (count > 0) {
+    steer.div(count);
+  }
+
+  // As long as the vector is greater than 0
+  if (steer.mag() > 0) {
+    // Implement Reynolds: Steering = Desired - Velocity
+    steer.normalize();
+    steer.mult(this.maxspeed);
+    steer.sub(this.velocity);
+    steer.limit(this.maxforce);
+  }
+  return steer;
+};
+
+// Alignment
+// For every nearby boid in the system, calculate the average velocity
+Lyric.prototype.align = function(lyrics) {
+  var neighbordist = 50;
+  var sum = createVector(0, 0);
+  var count = 0;
+  for (var i = 0; i < lyrics.length; i++) {
+    var d = p5.Vector.dist(this.position, lyrics[i].position);
+    if ((d > 0) && (d < neighbordist)) {
+      sum.add(lyrics[i].velocity);
+      count++;
+    }
+  }
+  if (count > 0) {
+    sum.div(count);
+    sum.normalize();
+    sum.mult(this.maxspeed);
+    var steer = p5.Vector.sub(sum, this.velocity);
+    steer.limit(this.maxforce);
+    return steer;
+  } else {
+    return createVector(0, 0);
+  }
+};
+
+// Cohesion
+// For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
+Lyric.prototype.cohesion = function(lyrics) {
+  var neighbordist = 50;
+  var sum = createVector(0, 0); // Start with empty vector to accumulate all locations
+  var count = 0;
+  for (var i = 0; i < lyrics.length; i++) {
+    var d = p5.Vector.dist(this.position, lyrics[i].position);
+    if ((d > 0) && (d < neighbordist)) {
+      sum.add(lyrics[i].position); // Add location
+      count++;
+    }
+  }
+  if (count > 0) {
+    sum.div(count);
+    return this.seek(sum); // Steer towards the location
+  } else {
+    return createVector(0, 0);
+  }
+};
+
+ 
---
+++ script.js
@@ -0,0 +1,111 @@
+var lyrics;
+
+var app = {
+    //Define the url for the wikipedia API call
+   //currentWord: "salad",
+  musixMatchRest:"&page_size=10&page=1&s_track_rating=desc",
+  musixMatchURL: "http://api.musixmatch.com/ws/1.1/track.search?&format=jsonp&f_lyrics_language=en&q_lyrics=",
+   //newCurrentWord: "salad",
+   //musixMatchRest:"&page_size=10&page=1&s_track_rating=desc",
+  musixMatchKey:"&apikey=d919d8d046718bd5469b63e886f873cc",
+   
+   //A place to set up listeners or kick off an initial function
+  initialize: function() {
+    $("#title").hide();
+    $('#myCanvasDiv').hide();
+    $("#search").click(function() {
+      $("#resultsTarget").html("");
+        //Use jQuery to get the value of the 'query' input box
+      var newCurrentWord = $("#query").val();
+      $("#title h1").html($("#title h1").html() + " " + newCurrentWord);
+    //console.log(newCurrentWord);
+    //Execute the MusixMatch API call function with the currentWord var as the argument
+      app.searchMusixMatch(app.newCurrentWord);
+    });
+  },    
+  searchMusixMatch: function(word) {
+    console.log("Executing the searchMusixMatch function"); 
+    var searchResults1, searchResults2, searchResults3;
+    $.ajax({
+      url: app.musixMatchURL + word /*+ app.musixMatchRest+ &format=jsonp& */ + app.musixMatchKey, //word is the argument passed into the function
+      type: 'GET',
+      dataType: 'jsonp',
+      error: function(data){
+        console.log("We got problems");
+        //console.log(data.status);
+      },
+      success: function(data) {
+        console.log("WooHoo!");
+          //Check the browser console to see the returned data
+        //title +=  $('#title').show();
+        //title +=  '</div>';
+
+        var findRandomTrackNumber = function() {
+          var randomNum = Math.floor(Math.random() * 10);
+          console.log(data.message.body.track_list[randomNum]);
+          if (!data.message.body.track_list || 
+              !data.message.body.track_list[randomNum] || 
+              !data.message.body.track_list[randomNum].track || 
+              !data.message.body.track_list[randomNum].track.track_name ||
+              data.message.body.track_list[randomNum].track.track_name.toLowerCase().indexOf('undefined') !== -1 ||
+              data.message.body.track_list[randomNum].track.artist_name.toLowerCase().indexOf('undefined') !== -1) {
+            return findRandomTrackNumber();
+          } else {
+            return randomNum;
+          }
+          
+        };
+
+        var randomTrackNumber = findRandomTrackNumber();
+      
+        searchResults1 = (data.message.body.track_list[randomTrackNumber].track.track_name);
+        searchResults2 = (data.message.body.track_list[randomTrackNumber].track.artist_name);
+        searchResults3 = (data.message.body.track_list[randomTrackNumber].track.track_id);
+
+        var htmlString= '<div class="Results">';
+         htmlString += '<h2>' + searchResults1 + '</h2>';
+         htmlString += '<span>' + searchResults2 + '</span>';
+         htmlString += '</div>';
+          $('#resultsTarget').append(htmlString);
+          $(".go-away").hide();
+          $('#myCanvasDiv').show();
+        
+      }
+    }).then(function () {
+      $.ajax({
+        url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get?&format=jsonp&track_id=" + searchResults3 + app.musixMatchKey,
+        type:'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        success: function(data) {
+          console.log("It worked!");
+          $('#everythingElse').hide();
+          $('#myCanvasDiv').show();
+        var tempLyrics= data.message.body.lyrics.lyrics_body; 
+        var str = tempLyrics;
+        lyrics = str.split(" ");
+        lyrics = _.filter(lyrics, function(lyric){
+            lyric = lyric.replace(/^[a-zA-Z]*$/,'');
+            if (!lyric.includes('undefined') && lyric !== 'and' && !lyric.includes('*') && lyric !== undefined) {
+                return lyric;
+            }
+        });
+    
+        loadLyricsOnce();
+        for (var i=0; i<lyrics.length; i++) {
+             console.log(lyrics[i]);
+
+        }
+
+      }
+    });   
+  });           
+  }
+};
+
+   
+$(document).ready(function(){
+  console.log("LOADED!!!!");
+  app.initialize();
+
+});  
